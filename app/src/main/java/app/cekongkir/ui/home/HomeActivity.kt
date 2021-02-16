007bf78f89ec8321fa80f5409a54b70f891a41c0
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-16 22:14:27 +0700

    change old ways to bind view with ViewBinding instead of findViewById

--- app/src/main/java/app/cekongkir/ui/home/HomeActivity.kt
+++ app/src/main/java/app/cekongkir/ui/home/HomeActivity.kt
@@ -2,28 +2,29 @@ package app.cekongkir.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import app.cekongkir.R
+import app.cekongkir.databinding.ActivityHomeBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeActivity : AppCompatActivity() {
 
-    val tabLayout by lazy { findViewById<TabLayout>(R.id.tab_layout) }
-    val viewPager by lazy { findViewById<ViewPager2>(R.id.view_pager) }
+    private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
         setupTab()
     }
 
     private fun setupTab() {
         val tabTitles = arrayOf("CEK ONGKIR", "CEK RESI")
         val tabAdapter = HomeTabAdapter(supportFragmentManager, lifecycle)
-        viewPager.adapter = tabAdapter
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+        binding.viewPager.adapter = tabAdapter
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             tab.text = tabTitles[position]
         }.attach()
     }
