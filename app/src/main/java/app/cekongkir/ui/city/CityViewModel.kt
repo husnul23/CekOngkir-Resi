86d905bf0a351fab0e7f9a833af86ea0536c3035
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-17 20:52:49 +0700

    add base CityViewModel

---
+++ app/src/main/java/app/cekongkir/ui/city/CityViewModel.kt
@@ -0,0 +1,10 @@
+package app.cekongkir.ui.city
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CityViewModel : ViewModel() {
+
+    val titleBar: MutableLiveData<String> = MutableLiveData("")
+
+}
