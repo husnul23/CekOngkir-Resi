f963a6113855ba2c979bef9f751d3832c69a4123
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-17 22:09:11 +0700

    add endpoint into ViewModel

--- app/src/main/java/app/cekongkir/ui/city/CityViewModel.kt
+++ app/src/main/java/app/cekongkir/ui/city/CityViewModel.kt
@@ -2,8 +2,12 @@ package app.cekongkir.ui.city
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import app.cekongkir.network.ApiService
+import app.cekongkir.network.RajaOngkirEndpoint
 
-class CityViewModel : ViewModel() {
+class CityViewModel(
+        val api: RajaOngkirEndpoint
+) : ViewModel() {
 
     val titleBar: MutableLiveData<String> = MutableLiveData("")
 
