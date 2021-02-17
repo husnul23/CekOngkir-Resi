7eb6b592d8a52e19e14c2fbaa017faa6bb661da2
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-17 21:35:06 +0700

    add data class CityResponse

---
+++ app/src/main/java/app/cekongkir/network/response/CityResponse.kt
@@ -0,0 +1,25 @@
+package app.cekongkir.network.response
+
+data class CityResponse(
+        val rajaongkir: RajaOngkir) {
+
+    data class RajaOngkir(
+            val query: List<Any>,
+            val status: Status,
+            val results: List<Results>
+    ) {
+        data class Status(
+                val code: Int,
+                val description: String
+        )
+
+        data class Results(
+                val city_id: String,
+                val province_id: String,
+                val province: String,
+                val type: String,
+                val city_name: String,
+                val postal_code: String
+        )
+    }
+}
