542d551606da5de1b82636ae72518ef92840ad4d
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-25 15:49:54 +0700

    update endpoint, adding subdistrict to get more data

--- app/src/main/java/app/cekongkir/network/RajaOngkirEndpoint.kt
+++ app/src/main/java/app/cekongkir/network/RajaOngkirEndpoint.kt
@@ -1,11 +1,18 @@
 package app.cekongkir.network
 
 import app.cekongkir.network.response.CityResponse
+import app.cekongkir.network.response.SubDistrictResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RajaOngkirEndpoint {
 
     @GET("city")
     suspend fun city(): Response<CityResponse>
+
+    @GET("subdistrict")
+    suspend fun subdistrict(
+            @Query("city") city: String
+    ) : Response<SubDistrictResponse>
 }
