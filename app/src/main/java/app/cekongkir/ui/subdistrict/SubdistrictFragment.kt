edbcdb5b006ca3c09abbda5fe45a98493b49fd93
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-02-28 20:17:31 +0700

    add observer to fragment

--- app/src/main/java/app/cekongkir/ui/subdistrict/SubdistrictFragment.kt
+++ app/src/main/java/app/cekongkir/ui/subdistrict/SubdistrictFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import app.cekongkir.databinding.FragmentSubdistrictBinding
+import app.cekongkir.network.Resource
+import app.cekongkir.network.response.SubDistrictResponse
 import app.cekongkir.ui.city.CityViewModel
+import timber.log.Timber
 
 class SubdistrictFragment : Fragment() {
 
     private val viewModel by lazy { ViewModelProvider(requireActivity()).get(CityViewModel::class.java) }
     private lateinit var binding: FragmentSubdistrictBinding
+    private lateinit var subdistrictAdapter: SubdistrictAdapter
+    private val cityId by lazy { requireArguments().getString("city_id") }
+    private val cityName by lazy { requireArguments().getString("city_name") }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -22,7 +30,47 @@ class SubdistrictFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupView()
+        setupRecyclerView()
+        setupListener()
+        setupObserver()
+    }
 
+    private fun setupRecyclerView() {
+        subdistrictAdapter = SubdistrictAdapter(arrayListOf(), object : SubdistrictAdapter.OnAdapterListener{
+            override fun onClick(result: SubDistrictResponse.Rajaongkir.Results) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        binding.listSubdistrict.adapter = subdistrictAdapter
+    }
+
+    private fun setupListener() {
+        binding.refreshSubdistrict.setOnRefreshListener{
+                viewModel.fetchSubDistrict( cityId!! )
+        }
+    }
+
+    private fun setupView() {
         viewModel.titleBar.postValue("Pilih Kecamatan")
+    }
+
+    private fun setupObserver() {
+        viewModel.subDistrictResponse.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is Resource.Loading -> binding.refreshSubdistrict.isRefreshing = true
+
+                is Resource.Success -> {
+                    binding.refreshSubdistrict.isRefreshing = false
+                    Timber.e("subDistrictResponse ${it.data!!.rajaongkir.results}")
+                    subdistrictAdapter.setData( it.data!!.rajaongkir.results)
+                }
+                is Resource.Error -> {
+                    binding.refreshSubdistrict.isRefreshing = false
+                    Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 }
