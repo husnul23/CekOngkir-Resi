91a8e0970104b3a1747797c60042edc81ef7f191
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-03-02 18:40:28 +0700

    add repository to adapter

--- app/src/main/java/app/cekongkir/ui/city/CityActivity.kt
+++ app/src/main/java/app/cekongkir/ui/city/CityActivity.kt
@@ -6,23 +6,25 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import app.cekongkir.R
+import app.cekongkir.database.preferences.CekOngkirPreference
 import app.cekongkir.network.ApiService
+import app.cekongkir.network.RajaOngkirRepository
 import app.cekongkir.network.Resource
 import timber.log.Timber
 
 class CityActivity : AppCompatActivity(){
 
     private val api by lazy { ApiService.getClient() }
+    private val pref by lazy { CekOngkirPreference(this) }
     private lateinit var viewModelFactory : CityViewModelFactory
     private lateinit var viewModel : CityViewModel
+    private lateinit var repository: RajaOngkirRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city)
         setupViewModel()
         setupObserver()
-
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -31,7 +33,8 @@ class CityActivity : AppCompatActivity(){
     }
 
     private fun setupViewModel() {
-        viewModelFactory = CityViewModelFactory(api)
+        repository = RajaOngkirRepository( api, pref )
+        viewModelFactory = CityViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(CityViewModel::class.java)
     }
 
