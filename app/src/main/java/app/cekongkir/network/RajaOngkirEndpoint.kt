ff9bcd57255c91b76f162cd0280c7350f850b13e
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-03-20 14:59:07 +0700

    Update RajaOngkirEndpoint.kt
    add post method to RajaOngkirEndpoint

--- app/src/main/java/app/cekongkir/network/RajaOngkirEndpoint.kt
+++ app/src/main/java/app/cekongkir/network/RajaOngkirEndpoint.kt
@@ -3,8 +3,7 @@ package app.cekongkir.network
 import app.cekongkir.network.response.CityResponse
 import app.cekongkir.network.response.SubDistrictResponse
 import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface RajaOngkirEndpoint {
 
@@ -15,4 +14,15 @@ interface RajaOngkirEndpoint {
     suspend fun subdistrict(
             @Query("city") city: String
     ) : Response<SubDistrictResponse>
+
+    @FormUrlEncoded
+    @POST("cost")
+    suspend fun cost(
+            @Field("origin") origin: String,
+            @Field("originType") originType: String,
+            @Field("destination") destination: String,
+            @Field("destinationType") destinationType: String,
+            @Field("weight") weight: String,
+            @Field("courier") courier: String,
+    )
 }
